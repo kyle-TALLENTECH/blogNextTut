12d3bc0e55f2e409f43bbd2a15357eccfacd9c1b
Kyle Carvalho <kyle@tallentech.com.au>
2024-10-13 22:12:13 +0800

    finished project- post fix lastresult error

--- app/dashboard/sites/[siteId]/create/page.tsx
+++ app/dashboard/sites/[siteId]/create/page.tsx
@@ -2,11 +2,16 @@
 
 import { CreatePostAction } from "@/app/actions";
 import TailwindEditor from "@/app/components/dashboard/EditorWrapper";
-import { SubmitButton } from "@/app/components/dashboard/SubmitButtons";
 import { UploadDropzone } from "@/app/utils/UploadthingComponents";
 import { PostSchema } from "@/app/utils/zodSchemas";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
@@ -17,139 +22,168 @@ import Image from "next/image";
 import Link from "next/link";
 import { JSONContent } from "novel";
 import { useActionState, useState } from "react";
-import slugify from "react-slugify";
 import { toast } from "sonner";
+import slugify from "react-slugify";
+import { SubmitButton } from "@/app/components/dashboard/SubmitButtons";
 
-export default function ArticleCreateRoute({
-    params,
-  }: {
-    params: { siteId: string };
-  }) {
-    console.log("Site ID: ", params.siteId); // This should print the correct `siteId` value
+export default function ArticleCreationRoute({
+  params,
+}: {
+  params: { siteId: string };
+}) {
+  const [imageUrl, setImageUrl] = useState<undefined | string>(undefined);
+  const [value, setValue] = useState<JSONContent | undefined>(undefined);
+  const [slug, setSlugValue] = useState<undefined | string>(undefined);
+  const [title, setTitle] = useState<undefined | string>(undefined);
+  const [lastResult, action] = useActionState(CreatePostAction, undefined);
+  const [form, fields] = useForm({
+    lastResult,
 
-    const [imageUrl, setImageUrl] = useState<undefined | string>(undefined);
-    const [value, setValue] = useState<JSONContent|undefined>(undefined);
-    const [title,setTitle] = useState<undefined|string>(undefined)
-    const [slug,setSlugValue] = useState<undefined|string>(undefined)
-    const [lastResult, action] = useActionState(CreatePostAction,undefined)
-    const [form, fields] = useForm({
-        lastResult: lastResult ? lastResult() : undefined,
-        onValidate({formData}){
-            return parseWithZod(formData, {schema:PostSchema})
-        },
-        shouldValidate: "onBlur",
-        shouldRevalidate: "onInput"
-    })
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: PostSchema });
+    },
 
-    function handleSlugGenration(){
-            const titleInput = title
-            if(titleInput === undefined || titleInput.length === 0){
-                return toast.error("Please enter a title")
-            }
-            setSlugValue(slugify(titleInput))
-            return toast.success("Slug has been generated")
-            }
-    return (
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
 
-        <>
-            <div className="flex items-center  ">
-                
-                <Button size="icon" variant="outline" className="mr-3" asChild>
-                    
-                <Link href={`/dashboard/sites/${params.siteId}`}>
+  function handleSlugGeneration() {
+    const titleInput = title;
 
-                        <ArrowLeft className="size-4" />
-                    </Link>
+    if (titleInput?.length === 0 || titleInput === undefined) {
+      return toast.error("Pleaes create a title first");
+    }
 
-                </Button>
-                <h1 className="text-xl font-semibold">Create Article</h1>
+    setSlugValue(slugify(titleInput));
 
+    return toast.success("Slug has been created");
+  }
+  return (
+    <>
+      <div className="flex items-center">
+        <Button size="icon" variant="outline" className="mr-3" asChild>
+          <Link href={`/dashboard/sites/${params.siteId}`}>
+            <ArrowLeft className="size-4" />
+          </Link>
+        </Button>
+        <h1 className="text-xl font-semibold">Create Article</h1>
+      </div>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Article Details</CardTitle>
+          <CardDescription>
+            Lipsum dolor sit amet, consectetur adipiscing elit
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            className="flex flex-col gap-6"
+            id={form.id}
+            onSubmit={form.onSubmit}
+            action={action}
+          >
+            <input type="hidden" name="siteId" value={params.siteId} />
+            <div className="grid gap-2">
+              <Label>Title</Label>
+              <Input
+                key={fields.title.key}
+                name={fields.title.name}
+                defaultValue={fields.title.initialValue}
+                placeholder="Nextjs blogging application"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+              />
+              <p className="text-red-500 text-sm">{fields.title.errors}</p>
             </div>
 
-            <Card>
-                <CardHeader>
-                    <CardTitle>Article Details</CardTitle>
-                    <CardDescription>Create a new site to get started and see them right here! Click the button below to start.</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form className="flex flex-col gap-6" id = {form.id} onSubmit = {form.onSubmit} action = {action}>
+            <div className="grid gap-2">
+              <Label>Slug</Label>
+              <Input
+                key={fields.slug.key}
+                name={fields.slug.name}
+                defaultValue={fields.slug.initialValue}
+                placeholder="Article Slug"
+                onChange={(e) => setSlugValue(e.target.value)}
+                value={slug}
+              />
+              <Button
+                onClick={handleSlugGeneration}
+                className="w-fit"
+                variant="secondary"
+                type="button"
+              >
+                <Atom className="size-4 mr-2" /> Generate Slug
+              </Button>
+              <p className="text-red-500 text-sm">{fields.slug.errors}</p>
+            </div>
 
-                        <input type="hidden" name="siteId" value={params.siteId} />
+            <div className="grid gap-2">
+              <Label>Small Description</Label>
+              <Textarea
+                key={fields.smallDescription.key}
+                name={fields.smallDescription.name}
+                defaultValue={fields.smallDescription.initialValue}
+                placeholder="Small Description for your blog article..."
+                className="h-32"
+              />
+              <p className="text-red-500 text-sm">
+                {fields.smallDescription.errors}
+              </p>
+            </div>
 
-                        <div className="grid gap-2">
-                            <Label>Title</Label>
-                            <Input key = {fields.title.key} 
-                            name = {fields.title.name}
-                            defaultValue = {fields.title.initialValue} 
-                            placeholder="Nextjs bloggin application"
-                            onChange={(e)=>{setTitle(e.target.value)}}
-                            value = {title}
-                            ></Input>
-                            <p className="text-red-500 text-sm">{fields.title.errors}</p>
-                        </div>
-                        <div className="grid gap-2">
-                            <Label>Slug</Label>
+            <div className="grid gap-2">
+              <Label>Cover Image</Label>
+              <input
+                type="hidden"
+                name={fields.coverImage.name}
+                key={fields.coverImage.key}
+                defaultValue={fields.coverImage.initialValue}
+                value={imageUrl}
+              />
+              {imageUrl ? (
+                <Image
+                  src={imageUrl}
+                  alt="Uploaded Image"
+                  className="object-cover w-[200px] h-[200px] rounded-lg"
+                  width={200}
+                  height={200}
+                />
+              ) : (
+                <UploadDropzone
+                  onClientUploadComplete={(res) => {
+                    setImageUrl(res[0].url);
+                    toast.success("Image has been uploaded");
+                  }}
+                  endpoint="imageUploader"
+                  onUploadError={() => {
+                    toast.error("Something went wrong...");
+                  }}
+                />
+              )}
 
-                            <Input key={fields.slug.key} name={fields.slug.name} 
-                            defaultValue= {fields.slug.initialValue} 
-                            placeholder="Article Slug"
-                            onChange={(e)=>{setSlugValue(e.target.value)}}
-                            value = {slug}
-                            ></Input>
-                            
-                            <Button onClick = {handleSlugGenration} className="w-fit" variant="secondary" type="button">
-                                <Atom className="mr-2 size-4" /> Generate Slug
-                            </Button>
-                            
-                            <p className="text-red-500 text-sm">{fields.slug.errors}</p>
-                        </div>
-                        <div className="grid gap-2">
-                            <Label>Small Description</Label>
-                            <Textarea 
-                            key = {fields.smallDescription.key}
-                             name = {fields.smallDescription.name}
-                             defaultValue = {fields.smallDescription.initialValue}
-                             className="h-32" placeholder="Small Description for blog...."></Textarea>
-                            <p className="text-red-500 text-sm">{fields.smallDescription.errors}</p>
-                        </div>
+              <p className="text-red-500 text-sm">{fields.coverImage.errors}</p>
+            </div>
 
-                        <div className="grid gap-2">
-                            <Label>Cover Image</Label>
-                            <input type="hidden" name={fields.coverImage.name} key={fields.coverImage.key} defaultValue={fields.coverImage.initialValue} value = {imageUrl} />
-                            {imageUrl ? (
-                                <Image
+            <div className="grid gap-2">
+              <Label>Article Content</Label>
+              <input
+                type="hidden"
+                name={fields.articleContent.name}
+                key={fields.articleContent.key}
+                defaultValue={fields.articleContent.initialValue}
+                value={JSON.stringify(value)}
+              />
+              <TailwindEditor onChange={setValue} initialValue={value ?? {}} />
+              <p className="text-red-500 text-sm">
+                {fields.articleContent.errors}
+              </p>
+            </div>
 
-                                    src={imageUrl}
-                                    alt="Uploaded Image"
-                                    className="object-cover w-[200] h-[200] rounded-lg"
-                                    width={200}
-                                    height={200} />
-
-                            ) : (
-                                <UploadDropzone onClientUploadComplete={
-                                    (res) => {
-                                        setImageUrl(res[0].url)
-                                        toast.success("Image has been uploaded");
-
-                                    }}
-                                    endpoint="imageUploader"
-                                    onUploadError={() => {
-                                        toast.error("Something went wrong with the upload...");
-                                    }}
-                                />
-                            )}
-                            <p className="text-red-500 text-sm">{fields.coverImage.errors}</p>
-                        </div>
-                        <div className="grid gap-2">
-                            <Label>Article Content</Label>
-                            <input type="hidden" name={fields.articleContent.name} key={fields.articleContent.key} defaultValue={fields.articleContent.initialValue} value = {JSON.stringify(value)} />
-                            <TailwindEditor onChange={setValue} initialValue={value ?? {}} />
-                            <p className="text-red-500 text-sm">{fields.articleContent.errors}</p>
-                        </div>
-                        <SubmitButton text="Create Article" />
-                    </form>
-                </CardContent>
-            </Card>
-        </>
-    )
+            <SubmitButton text="Create Article" />
+          </form>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
